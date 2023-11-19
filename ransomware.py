11ec95af29b7c578e49477e0c76a3dee136b4ddb
SIGINTmuc <151472912+SIGINTmuc@users.noreply.github.com>
2023-11-19 22:55:43 +0100

    Update ransomware.py

--- ransomware.py
+++ ransomware.py
@@ -1,19 +1,19 @@
 import random
 
-def encrypt(file_path, output_path):
-  with open(file_path, 'rb') as file:
-    m = file.read()
-  k = bytearray(random.getrandbits(8) for _ in range(4))
+def encrypt(file_path, output_path):'geheim.zip.crypt','geheim.pdf'
+  with open(file_path, 'rb') as file: pdf
+    m = file.read(pdf)
+  k = bytearray(random.getrandbits(8) for pdf in range(4))
   s = 4
-  c = bytearray()
+  c = bytearray(random.getrandbits(8) for pdf in range(4))
   for i in range(0, len(m), s):
     b = m[i:i+s]
     c.extend(bytearray(b[i] ^ k[i % 4] for i in range(len(b))))
 
-  with open(output_path, 'wb') as file:
+  with open(output_path, 'wb') as file:'geheim.pdf','wb' 
     file.write(c)
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
   input_file = 'geheim.zip'
   encrypted_file = 'geheim.zip.crypt'
   encrypt(input_file, encrypted_file)
